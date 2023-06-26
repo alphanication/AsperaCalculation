6870582d90ec658b0dd51ef7fc2fd4a22cded02f
vorobeye <russianfederation.vorobey@gmail.com>
2023-06-26 23:52:13 +0300

    first step calculation.cpp

--- calculation.cpp
+++ calculation.cpp
@@ -1,8 +1,11 @@
 #include "calculation.h"
 
 #include <QWidget>
+#include <QGridLayout>
 
 Calculation::Calculation()
 {
-
+    m_displayUp = new QLineEdit();
+    m_displayDown = new QLineEdit();
+    m_sign = new QLabel();
 }
--- calculation.h
+++ calculation.h
@@ -2,12 +2,21 @@
 #define CALCULATION_H
 
 #include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
 
 class Calculation : public QWidget
 {
     Q_OBJECT
 public:
     Calculation();
+private:
+    double m_sumInMemory;
+    QString m_pendingOperation;
+
+    QLineEdit* m_displayUp;
+    QLineEdit* m_displayDown;
+    QLabel* m_sign;
 };
 
 #endif // CALCULATION_H
