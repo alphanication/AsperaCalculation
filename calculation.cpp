dfc9d67874f8e9c808c6f4cf389f846d4970b35e
vorobeye <russianfederation.vorobey@gmail.com>
2023-07-02 23:06:36 +0300

    alphanication, aspera calculation

--- calculation.cpp
+++ calculation.cpp
@@ -3,6 +3,8 @@
 #include <QWidget>
 #include <QGridLayout>
 
+// alphanication, aspera calculation
+
 Calculation::Calculation()
 {
     setWindowTitle("AsperaCalculation");
@@ -20,11 +22,20 @@ Calculation::Calculation()
     m_displayUp->setMaxLength(15);
     m_displayDown->setMaxLength(15);
 
+    QFont fontDisplayUp = m_displayUp->font();
+    fontDisplayUp.setPointSize(fontDisplayUp.pointSize() + 8);
+    m_displayUp->setFont(fontDisplayUp);
+
+    QFont fontDisplayDown = m_displayDown->font();
+    fontDisplayDown.setPointSize(fontDisplayDown.pointSize() + 8);
+    m_displayDown->setFont(fontDisplayDown);
+
     QGridLayout* mainLayout = new QGridLayout;
 
     mainLayout->addWidget(m_displayUp, 0, 0, 6, 1);
     mainLayout->addWidget(m_sign, 1, 5, 1, 1);
     mainLayout->addWidget(m_displayDown, 2, 0, 6, 1);
 
+    mainLayout->setSizeConstraint(QLayout::SetFixedSize);
     setLayout(mainLayout);
 }
